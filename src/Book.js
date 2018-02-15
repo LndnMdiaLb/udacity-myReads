32e845f03cf07c292d24415d2f7c3716620d1920
LndnMdiaLb <projects@thelondonmedialab.com>
2018-02-15 14:56:15 +0000

    Add Book Component
    added use of props.children

--- src/Book.js
+++ src/Book.js
@@ -1,26 +1,39 @@
 import React, {Component} from 'react' ;
+import PropTypes from 'prop-types' ;
 import './Book.css' ;
 
 class Book extends Component{
 
+    static propTypes = {
+        bookData: PropTypes.object ,
+        updateShelf: PropTypes.func
+    }
+
+    // componentDidMount(){
+
+    // }
+
     render(){
-        let info = this.props.info ;
+        let {bookData} = this.props ;
+        // console.log(bookData.id)
         return (
             <div className='book'>
                 <div className='thumb'>
                     <img
-                        src={ info.imageLinks.smallThumbnail }
+                        src={ bookData.imageLinks? bookData.imageLinks.smallThumbnail : '' }
                         alt = 'thumbnail'
                         />
                 </div>
                 <div>
-                    <h3>{info.title}</h3>
-                    <h5>{info.authors}</h5>
+                    <h3>{bookData.title}</h3>
+                    <h5>{bookData.authors}</h5>
                 </div>
+                {
+                    this.props.children
+                }
             </div>
         ) ;
     }
-
 }
 
 export default Book ;
