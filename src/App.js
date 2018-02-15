05625e305167553050fea7cad37a8d182b10ae02
LndnMdiaLb <projects@thelondonmedialab.com>
2018-02-15 14:53:09 +0000

    BooksAPI.js bug
    Can no longer use the update method.  Returned JSON never includes the passed book.id in the passed shelf property Array.
    Have added a series of buttons and console logs to track what data is being passed back and forth

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react' ;
-import PropTypes from 'prop-types';
-import {Route} from 'react-router-dom';
+import PropTypes from 'prop-types' ;
+import {Route, Link} from 'react-router-dom' ;
 import * as BooksAPI from './BooksAPI' ;
 import Shelf from './Shelf';
+import SearchForm from './SearchForm';
 import './App.css' ;
 
 class App extends Component {
@@ -11,17 +12,26 @@ class App extends Component {
     shelves:PropTypes.array
   }
 
-  // constructor(props){
-  //   super(props);
-  //   this.props.shelves = [ 'currentlyReading', 'wantToRead', 'read' ]; // error
-  // }
+
+    // Component's constructor
+  constructor(props){
+    // Required to call original constructor
+    super(props);
+    // Props are now accessible from here
+    // var v = props.hello_i_am_a_prop;
+    this.getAll = this.getAll.bind(this);
+    this.search = this.search.bind(this);
+    this.update = this.update.bind(this);
+  }
 
   state = {
+    // home page
     shelves:{
       currentlyReading:[],
       wantToRead:[],
       read:[]
     },
+    // search page
     searchResults:[]
   }
 
@@ -29,82 +39,132 @@ class App extends Component {
     Single Responsibility Principle - a component should ideally do one thing
   */
 
+  // does not mount everytime Routing is applied
   componentDidMount() {
     this.getAll() ;
   }
 
+  componentDidUpdate(){
+    console.log('did', this.state)
+  }
 
-// BooksAPI.update({id:"H8tNBKmPO5UC"},'currentlyReading').then(function(books){
-//   console.log(books);
-// })
+  /*
+  returns JSON list of all books on Shelves
+  */
 
   getAll(){
     BooksAPI.getAll()
       .then(books => {
-
           // construct initial shelves state in temp object
           const shelves = {} ;
           books.forEach(
             book => ( shelves[book.shelf] || (shelves[book.shelf] = []) ).push(book)
           ) ;
-
+          console.log('getall', shelves)
           this.setState({
             shelves
           }) ;
       })
   }
 
-  search(query){
-    query ?
-    // if query is not null/undefined : make api call with query
-    BooksAPI.search(query)
-      .then(searchResults => {
-        this.setState({
-          searchResults
-        }) ;
-      }) :
-    // if query is null/undefined : empty search results
-    this.setState({
-      searchResults:[]
-    }) ;
+  get(id){
+    BooksAPI.get(id)
+    .then(book => {
+        console.log('get book:', book)
+    })
   }
 
+  /*
+  */
+
+  search(query){
+    query ?
+      // if query is not null/undefined : make api call with query
+      BooksAPI.search(query)
+        .then(searchResults => {
+          // FIX
+          Array.isArray(searchResults) ?
+            this.setState({ searchResults }) :
+            this.setState({ searchResults:[] })
+        })
+        .catch(error => {
+          // state query error
+          this.setState({
+            searchResults:[]
+          }) ;
+        })
+      : // if query is null/undefined : empty search results
+      this.setState({
+        searchResults:[]
+      }) ;
+  }
+
+
   update(book, shelf){
+    /*
+    returns JSON list of all books.id on Shelves
+        {
+          currentlyReading:[<stringOfID>,<stringOfID>],
+          wantToRead:[<stringOfID>,<stringOfID>,<stringOfID>],
+          read:[<stringOfID>]
+        }
+    */
     BooksAPI.update(book.id, shelf)
-      .then(searchResults => {
+      .then(updates => {
+
+        console.log('update', updates) ;
         this.setState({
-          searchResults
+          updates
         }) ;
       })
   }
 
-
   render() {
     const shelfNames = Object.keys(this.state.shelves) ;
-
+    console.log(Object.keys(this.state.shelves)[1])
     return (
-
       <article>
-
         <h1>My Reads</h1>
 
-        <Route exact path='/' render={()=>
+        <section className='shelves'>
+
+          <Route exact path='/' render={() =>
+          <div>{
             shelfNames.map( shelf => (
-                  <Shelf key={shelf} title={shelf} books={this.state.shelves[shelf]}/>
-                )
-          )} />
+              <Shelf
+                key={ shelf }
+                name={shelf}
+                books={this.state.shelves[shelf]}
+                {...this.state}
+                update={this.update}>
+                  <h1>{shelf}</h1>
+              </Shelf>
+              ) )}
+                <Link to="/search">About</Link>
+                <button onClick={_=>this.update(this.state.searchResults[7], Object.keys(this.state.shelves)[1])}>update</button>
+                <button onClick={_=>this.getAll()}>get</button>
+                <button onClick={_=>this.get(this.state.searchResults[7].id)}>get book</button>
+                <button onClick={_=>this.search('An')}>search</button>
+                </div>
 
-         <Route exact path='/search' render={()=> (
-            <div>
-              <input className='search' type='text' />
-              <Shelf books={ this.state.searchResults}/>
-            </div>
-          )} />
+            } />
 
+          <Route exact path='/search' render={() => (
+              <Shelf
+                  name='search'
+                  books={this.state.searchResults}
+                  {...this.state}
+                  update={this.update}>
+                  <SearchForm
+                    search={ this.search }
+                    />
+              </Shelf>
+            )} />
+
+          </section>
       </article>
     )
   }
-
 }
 
 export default App ;
